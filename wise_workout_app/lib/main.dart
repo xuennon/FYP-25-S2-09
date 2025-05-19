c9420c870ca4751237e5bea438b40083f647ea57
xuennon <lockshannon@hotmail.com>
2025-05-19 21:58:47 +0800

    test1

--- wise_workout_app/lib/main.dart
+++ wise_workout_app/lib/main.dart
@@ -1,20 +1,52 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
+import 'package:flutter/material.dart'; 
+ 
+void main() => runApp(MyApp()); 
+ 
+// ignore: must_be_immutable 
+class MyApp extends StatelessWidget { 
+  var subject = 'English'; 
+ 
+  MyApp({Key? key}) : super(key: key); 
+ 
+  @override 
+  Widget build(BuildContext context) { 
+    return MaterialApp( 
+      home: Scaffold( 
+        appBar: AppBar( 
+          title: const Text('Stateful Widget'), 
+        ), 
+        body: Column( 
+          children: [ 
+            const Text('What is your favorite subject?'), 
+            ElevatedButton( 
+              style: ElevatedButton.styleFrom( 
+                backgroundColor: Colors.teal, 
+                foregroundColor: Colors.white, 
+              ), 
+              onPressed: () => subject = 'English', 
+              child: const Text('English'), 
+            ), 
+            ElevatedButton( 
+              style: ElevatedButton.styleFrom( 
+                backgroundColor: Colors.teal, 
+                foregroundColor: Colors.white, 
+              ), 
+              onPressed: () => subject = 'Mathematics', 
+              child: const Text('Mathematics'), 
+            ), 
+            ElevatedButton( 
+              style: ElevatedButton.styleFrom( 
+                backgroundColor: Colors.teal, 
+                foregroundColor: Colors.white, 
+              ), 
+              onPressed: () => subject = 'Science', 
+              child: const Text('Science'), 
+            ), 
+            Text('My favorite subject is ' + subject), 
+          ], 
+        ), 
+      ), 
+    ); 
+  } 
+}  
+ 
