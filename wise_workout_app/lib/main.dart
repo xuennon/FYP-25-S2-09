528709b70beb9b1239f9f339a4b7cebf6f5b5091
ddevMetal <145380370+ddevMetal@users.noreply.github.com>
2025-05-26 23:06:00 +0800

    i change the mama

--- wise_workout_app/lib/main.dart
+++ wise_workout_app/lib/main.dart
@@ -1,52 +1,49 @@
-import 'package:flutter/material.dart'; 
- 
-void main() => runApp(MyApp()); 
- 
-// ignore: must_be_immutable 
-class MyApp extends StatelessWidget { 
-  var subject = 'English'; 
- 
-  MyApp({Key? key}) : super(key: key); 
- 
-  @override 
-  Widget build(BuildContext context) { 
-    return MaterialApp( 
-      home: Scaffold( 
-        appBar: AppBar( 
-          title: const Text('Stateful Widget'), 
-        ), 
-        body: Column( 
-          children: [ 
-            const Text('What is your favorite subject?'), 
-            ElevatedButton( 
-              style: ElevatedButton.styleFrom( 
-                backgroundColor: Colors.teal, 
-                foregroundColor: Colors.white, 
-              ), 
-              onPressed: () => subject = 'English', 
-              child: const Text('English'), 
-            ), 
-            ElevatedButton( 
-              style: ElevatedButton.styleFrom( 
-                backgroundColor: Colors.teal, 
-                foregroundColor: Colors.white, 
-              ), 
-              onPressed: () => subject = 'Mathematics', 
-              child: const Text('Mathematics'), 
-            ), 
-            ElevatedButton( 
-              style: ElevatedButton.styleFrom( 
-                backgroundColor: Colors.teal, 
-                foregroundColor: Colors.white, 
-              ), 
-              onPressed: () => subject = 'Science', 
-              child: const Text('Science'), 
-            ), 
-            Text('chnaginggg ' + subject), 
-          ], 
-        ), 
-      ), 
-    ); 
-  } 
-}  
- 
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// ignore: must_be_immutable
+class MyApp extends StatelessWidget {
+  var subject = 'English';
+
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Stateful Widget')),
+        body: Column(
+          children: [
+            const Text('What is your favorite subject?'),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () => subject = 'English',
+              child: const Text('chao chee bai '),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () => subject = 'Mathematics',
+              child: const Text('Mathematics'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () => subject = 'Science',
+              child: const Text('Science'),
+            ),
+            Text('change ni ma ma' + subject),
+          ],
+        ),
+      ),
+    );
+  }
+}
