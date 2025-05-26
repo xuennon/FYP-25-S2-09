01ae13fe351afc38e508bc77e6847470d9b3eaac
xuennon <lockshannon@hotmail.com>
2025-05-26 22:42:06 +0800

    testing2

--- wise_workout_app/lib/main.dart
+++ wise_workout_app/lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
               onPressed: () => subject = 'Science', 
               child: const Text('Science'), 
             ), 
-            Text('Mhiiiiiiis ' + subject), 
+            Text('chnaginggg ' + subject), 
           ], 
         ), 
       ), 
