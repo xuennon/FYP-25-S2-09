d65316035791a7aa100ab4154e7c648123f3129a
xuennon <lockshannon@hotmail.com>
2025-08-05 12:52:14 +0800

    fix arrow issue

--- wise_workout_app/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ wise_workout_app/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -31,6 +31,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin firebase_core, io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new io.flutter.plugins.googlesignin.GoogleSignInPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin google_sign_in_android, io.flutter.plugins.googlesignin.GoogleSignInPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.urllauncher.UrlLauncherPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin url_launcher_android, io.flutter.plugins.urllauncher.UrlLauncherPlugin", e);
